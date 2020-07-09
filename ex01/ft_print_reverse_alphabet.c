672c123372aa940c443a1c71146dafbf4dd6c107
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r4s3.wethinkcode.co.za>
2020-07-09 16:30:02 +0200

    pushing correction of ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,34 +6,25 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 12:19:44 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/09 12:57:14 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/09 16:28:37 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
 
-//commented out
-/*
-void ft_putchar(char c){
-	write(1,&c,1);
-}*/
+#include <unistd.h>
 
 void ft_print_reverse_alphabet(){
 	
-	char c;
-	for(c = 'z'; c >= 'a'; c--){
-		ft_putchar(c);
+	char begin;
+	char end;
+
+	begin = 'z';
+	end = 'a';
+
+	while(begin >= end){
+		ft_putchar(begin);
+		begin--;
 	}
 
 	ft_putchar('\n');
 }
-
-//commented out
-/*
-
-int main()
-{
-	ft_print_reverse_alphabet();
-	return (0);
-}*/
