7d73522aa8a81c7123e467922e478f54e275c17a
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r4s3.wethinkcode.co.za>
2020-07-09 15:53:01 +0200

    ex00 correction

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,37 +6,28 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 08:27:50 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/09 11:58:52 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/09 15:51:43 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
-
-// commented out as it s not required in the solution
-/*void ft_putchar(char c)
-{
-	
-	write(1, &c, 1);
-}*/
-
-
 
 void ft_print_alphabet()
 {
-	char c;
+	char begin;
+	char end;
 
-	for (char c = 'a' ; c <= 'z'; c++){
-		ft_putchar(c);
+	begin = 'a';
+	end = 'z';
+
+	while(begin <= end){
+
+		ft_putchar(begin);
+		begin++;
 	}
 
 	ft_putchar('\n');
 
 }
 
-//commented out as it is not required in the solution
-/*int main()
-{
-	ft_print_alphabet();
-	return (0);
-}*/
+
