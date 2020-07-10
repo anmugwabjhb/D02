7532c6b68d39847c9752806c185dc8bf2ae618f7
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-10 08:34:13 +0200

    ex00 finished

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,11 +6,11 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 08:27:50 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/10 08:31:49 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/10 08:33:34 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_print_alphabet()
+void ft_print_alphabet(void)
 {
 	char begin;
 	char end;
