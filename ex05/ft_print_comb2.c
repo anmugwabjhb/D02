543d6ceea8fdcf7125ffcd171d52c967f274fea0
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-10 09:53:27 +0200

    corrections

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -1,53 +1,69 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/10 08:40:32 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/10 09:52:19 by anmugwab         ###   ########.fr       */
+/*   Created: 2020/07/10 09:11:01 by anmugwab          #+#    #+#             */
+/*   Updated: 2020/07/10 09:32:47 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_print_comb(void)
+#include <unistd>
+
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
+
+void ft_print_comb2(void)
 {
 	char first;
 	char second;
 	char third;
+	char fourth;
 
-	first = 48;
-	second = 48;
-	third = 48;
+	first = '0';
+	second = '0';
+	third = '0';
+	fourth = '0';
 
-	while (first <= 57)
+	while (first <= '9')
 	{
-		while (second <= 57)
+		while (second <= '9')
 		{
-			while (third <= 57)
+			while (third <= '9')
 			{
-				if (first < second && second < third)
+				while (fourth <= '9')
 				{
-					ft_putchar(first);
-					ft_putchar(second);
-					ft_putchar(third);
-
-					if (first < 55)
+					if (first < third)
 					{
+						ft_putchar(first);
+						ft_putchar(second);
+						ft_putchar(' ');
+						ft_putchar(third);
+						ft_putchar(fourth);
 						ft_putchar(',');
 						ft_putchar(' ');
 					}
-					else
-						ft_putchar('\n');
-					
+
+					fouth++;
 				}
+				fourth = 0;
 				third++;
+
 			}
 			third = 0;
 			second++;
 		}
 		second = 0;
-		first++;;
+		first++;
 	}
+}
 
+int main(void)
+{
+	ft_print_comb2();
+	return (0);
 }
