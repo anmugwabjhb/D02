68803fbbe9d719ab14185cf685de41a742b8ec5f
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-10 08:26:42 +0200

    correction on ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,14 +6,12 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 12:19:44 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/09 16:28:37 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/10 08:25:20 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <unistd.h>
-
-void ft_print_reverse_alphabet(){
+void ft_print_reverse_alphabet(void)
+{
 	
 	char begin;
 	char end;
