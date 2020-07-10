dbbe2818d3b6ba550cbb7bebdf90d132d4ea86c8
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-10 08:32:35 +0200

    ex00 correction

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,11 +6,9 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 08:27:50 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/09 15:51:43 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/10 08:31:49 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 void ft_print_alphabet()
 {
