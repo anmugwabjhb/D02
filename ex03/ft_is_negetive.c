83edeecae99f157886428aa1f6c966c6e4fe3113
Anza Tshilidzi Michael Mugwabana <anmugwab@c5r6s3.wethinkcode.co.za>
2020-07-10 08:37:04 +0200

    ex03 finished

--- ex03/ft_is_negetive.c
+++ ex03/ft_is_negetive.c
@@ -6,11 +6,12 @@
 /*   By: anmugwab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 16:58:38 by anmugwab          #+#    #+#             */
-/*   Updated: 2020/07/10 08:18:58 by anmugwab         ###   ########.fr       */
+/*   Updated: 2020/07/10 08:36:29 by anmugwab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_is_negetive(int n){
+void ft_is_negetive(int n)
+{
 	if (n < 0)
 		ft_putchar('N');
 	else
